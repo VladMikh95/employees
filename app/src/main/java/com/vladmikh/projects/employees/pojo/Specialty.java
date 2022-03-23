2eef426aabc29da22971251a77bfe8fc4a0915a4
Vlad <vlad.mikhnenok.95@mail.ru>
2022-03-23 18:09:38 +0300

    Dependencies RxJava, Retrofit, GSON have been added to the project, pojo classes have been created

---
+++ app/src/main/java/com/vladmikh/projects/employees/pojo/Specialty.java
@@ -0,0 +1,29 @@
+package com.vladmikh.projects.employees.pojo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Specialty {
+    @SerializedName("specialty_id")
+    @Expose
+    private Integer specialtyId;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public Integer getSpecialtyId() {
+        return specialtyId;
+    }
+
+    public void setSpecialtyId(Integer specialtyId) {
+        this.specialtyId = specialtyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
